2c4e2017ea5031f29e6c580680512196cb030f51
Will9214 <129124730+Will9214@users.noreply.github.com>
2023-09-26 16:58:22 -0400

    added try...catch statements to controllers in order to handle errors.

--- controllers/flashcard.js
+++ controllers/flashcard.js
@@ -1,29 +1,41 @@
 const Flashcard = require("../models/flashcard");
 
-const getFlashcards = async function (req, res, next) {
-  const flashcards = await Flashcard.find();
+const getFlashcards = async function(req, res, next) {
+  try {
+    const flashcards = await Flashcard.find();
 
-  res.send({ flashcards });
+    res.send({ flashcards });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const addFlashcard = async function (req, res, next) {
-  const { name, info } = req.body;
+  try {
+    const { name, info } = req.body;
 
-  const flashcard = new Flashcard({
-    name: name,
-    info: info,
-  });
+    const flashcard = new Flashcard({
+      name: name,
+      info: info,
+    });
 
-  await flashcard.save();
+    await flashcard.save();
 
-  res.status(200).send({ flashcard });
+    res.status(200).send({ flashcard });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getRandomFlashcard = async function (req, res, next) {
-  const randomFlashcard = await Flashcard.aggregate([{$sample: { size: 1 }}]);
-
-  res.send({ randomFlashcard });
-}
+  try {
+    const randomFlashcard = await Flashcard.aggregate([{$sample: { size: 1 }}]);
+  
+    res.send({ randomFlashcard });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   getFlashcards,
